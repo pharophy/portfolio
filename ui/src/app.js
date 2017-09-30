6d7d0ac7f4c100c06d0d6636b36b83afa09136ec
Shawn Souto <shawn_souto@colpal.com>
2017-09-30 10:33:42 -0300

    add object to model for resume link

--- ui/src/app.js
+++ ui/src/app.js
@@ -12,5 +12,6 @@ export class App {
     this.person = this.personContext.getPerson(0);
     this.navigationLinks = this.navigationLinkContext.getNavigationLinks(this.person.id);
     //this.skills = _(this.navigationLinks).find(n => n.type === 'skill');
+    this.resumeLink = this.navigationLinks[7]; //TODO: when inet access, import lodash and change to _(this.navigationLinks).find(n => n.type === 'attachment');
   }
 }
