3fac29c64cb015bc518eb57da6bab2acdc6d777f
Shawn Souto <shawn.souto@gmail.com>
2018-06-16 20:09:26 -0400

    fix comma in skill.js

--- ui/src/models/skill.js
+++ ui/src/models/skill.js
@@ -1,4 +1,4 @@
-import NavigationLink from './navigation-link';
+import NavigationLink from 'models/navigation-link';
 
 export default class Skill extends NavigationLink {
   constructor(name, url, type, cssClass, orderIndex, description, content, id) {
@@ -18,7 +18,7 @@ export default class Skill extends NavigationLink {
       new Skill('Visionary', '/person/1/visionary', 'skill', 'ui-menu-color02', 1, 'This is a visionary description.'),
       new Skill('Enterprise Architect', '/engineer', 'skill', 'ui-menu-color03', 2, 'This is an architect description'),
       new Skill('Full Stack Engineer', '/engineer', 'skill', 'ui-menu-color03', 3, 'This is an engineer description'),
-      new Skill('Marketer', '/marketer', 'skill', 'ui-menu-color04', 4, 'This is a marketer description'),
+      new Skill('Marketer', '/marketer', 'skill', 'ui-menu-color04', 4, 'This is a marketer description')
     ];
   }
 }
