60cbeab6de78082acd181d3993292a42ab6bbbaf
Shawn Souto <shawn.souto@gmail.com>
2018-06-28 22:18:32 -0400

    add base pages for skill, portfolio, and history

--- ui/src/app.js
+++ ui/src/app.js
@@ -4,7 +4,10 @@ export class App {
   configureRouter(config, router) {
     config.map([
       //{ route: '',  moduleId: 'components/page',   title: 'Page'},
-      { route: '',  moduleId: 'person/summary',   title: 'Summary'}
+      { route: '',  moduleId: 'person/summary',   title: 'Summary'},
+      { route: 'history',  moduleId: 'person/history',   title: 'History'},
+      { route: 'portfolio',  moduleId: 'person/portfolio',   title: 'Portfolio'},
+      { route: 'skill',  moduleId: 'skills/detail',   title: 'Skill Information'}
       //{ route: ['', 'home'],   name: 'home',    moduleId: PLATFORM.moduleName('home') }
     ]);
 
