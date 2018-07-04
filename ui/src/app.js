2a8e0d7f4aca0d834be44e7f2dd4540874800f49
Shawn Souto <shawn.souto@gmail.com>
2018-07-03 20:46:42 -0400

    retitle routes

--- ui/src/app.js
+++ ui/src/app.js
@@ -6,11 +6,12 @@ export class App {
       //{ route: '',  moduleId: 'home',   title: 'Home'},
 
       //NOTE: must navigate to localhost:9000/#/person/1 to see home page
-      { route: ['/person/:id', '/person/:id/summary'],  moduleId: 'person/summary',   title: 'Summary'},
-      { route: 'person/:id/history',  moduleId: 'person/history',   title: 'History'},
-      { route: 'person/:id/portfolio',  moduleId: 'person/portfolio',   title: 'Portfolio'},
-      { route: 'person/:id/skills',  moduleId: 'person/skill',   title: 'Skill Information'},
-      { route: 'skill/:skill',  moduleId: 'skills/detail',   title: 'Skill Information'}
+      { route: ['/person/:id', '/person/:id/summary'],  moduleId: 'person/summary',   title: 'About Me'},
+      { route: 'person/:id/history',  moduleId: 'person/history',   title: 'My Work History'},
+      { route: 'person/:id/portfolio',  moduleId: 'person/portfolio',   title: 'My Portfolio'},
+      { route: 'person/:id/skills',  moduleId: 'person/skill',   title: 'My Skills'},
+      { route: 'person/:id/contact',  moduleId: 'person/contact',   title: 'Contact Me'},
+      { route: 'skill/:skill',  moduleId: 'skills/detail',   title: 'Skill Details'}
     ]);
 
     this.router = router;
