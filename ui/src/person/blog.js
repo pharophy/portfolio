76f60d5fed9d380c1cbebff28a4a57ffbe46f6a4
Shawn Souto <shawn.souto@gmail.com>
2018-07-03 22:56:35 -0400

    make blog content dynamic

--- ui/src/person/blog.js
+++ ui/src/person/blog.js
@@ -1,3 +1,15 @@
+import personContext from 'models/person';
+import blogContext from 'models/blog-entry';
 export default class Blog {
+  constructor() {
+    this.personContext = personContext;
+    this.blogContext = blogContext;
+    this.person = {};
+    this.blogEntries = [];
+  }
 
+  created() {
+    this.person = this.personContext.getPerson(0);
+    this.blogEntries = this.blogContext.getBlogEntries(this.person.id);
+  }
 }
