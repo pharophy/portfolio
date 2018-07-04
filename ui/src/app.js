d52c1051999338ec738457bf5186802bcb74368f
Shawn Souto <shawn.souto@gmail.com>
2018-07-03 21:47:15 -0400

    update routes to use new path to shared layout

--- ui/src/app.js
+++ ui/src/app.js
@@ -6,12 +6,12 @@ export class App {
       //{ route: '',  moduleId: 'home',   title: 'Home'},
 
       //NOTE: must navigate to localhost:9000/#/person/1 to see home page
-      { route: ['/person/:id', '/person/:id/summary'],  moduleId: 'person/summary',   title: 'About Me'},
-      { route: 'person/:id/history',  moduleId: 'person/history',   title: 'My Work History'},
-      { route: 'person/:id/portfolio',  moduleId: 'person/portfolio',   title: 'My Portfolio'},
-      { route: 'person/:id/skills',  moduleId: 'person/skill',   title: 'My Skills'},
-      { route: 'person/:id/contact',  moduleId: 'person/contact',   title: 'Contact Me'},
-      { route: 'skill/:skill',  moduleId: 'skills/detail',   title: 'Skill Details'}
+      { route: ['/person/:id', '/person/:id/summary'],  moduleId: 'person/summary',   title: 'About Me', layoutView: 'components/_shared/_layout.html'},
+      { route: 'person/:id/history',  moduleId: 'person/history',   title: 'My Work History', layoutView: 'components/_shared/_layout.html' },
+      { route: 'person/:id/portfolio',  moduleId: 'person/portfolio',   title: 'My Portfolio', layoutView: 'components/_shared/_layout.html' },
+      { route: 'person/:id/skills',  moduleId: 'person/skill',   title: 'My Skills', layoutView: 'components/_shared/_layout.html' },
+      { route: 'person/:id/contact',  moduleId: 'person/contact',   title: 'Contact Me', layoutView: 'components/_shared/_layout.html' },
+      { route: 'skill/:skill',  moduleId: 'skills/detail',   title: 'Skill Details', layoutView: 'components/_shared/_layout.html'}
     ]);
 
     this.router = router;
