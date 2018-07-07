c5ee571be5d472600cd6343d1b9168ff019a2114
Shawn Souto <shawn.souto@gmail.com>
2018-07-07 18:28:37 -0400

    add backend logic for skills and roles

--- ui/src/person/skill.js
+++ ui/src/person/skill.js
@@ -1,3 +1,20 @@
-export default class Skill {
+import personContext from 'models/person';
+import skillContext from 'models/skill';
+import _ from 'lodash';
 
+export default class Skill {
+  constructor() {
+    this.personContext = personContext;
+    this.skillContext = skillContext;
+    this.person = {};
+    this.roles = [];
+    this.skills = [];
+  }
+
+  created() {
+    this.person = this.personContext.getPerson(0);
+    const skillSet = this.skillContext.getSkills(this.person.id);
+    this.roles = _(skillSet).filter(n => n.subtype === 'role').value();
+    this.skills = _(skillSet).filter(n => n.subtype === 'technical').value();
+  }
 }
