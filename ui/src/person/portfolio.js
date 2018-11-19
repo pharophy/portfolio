8272c83f2123de88f05654d921fbff04cedb36e6
Shawn Souto <shawn.souto@gmail.com>
2018-11-18 23:01:00 -0500

    fix portfolio class name to Porfolio

--- ui/src/person/portfolio.js
+++ ui/src/person/portfolio.js
@@ -1,6 +1,6 @@
 import personContext from 'models/person';
 import portfolioContext from 'models/portfolio-entry';
-export default class Blog {
+export default class Portfolio {
   constructor() {
     this.personContext = personContext;
     this.portfolioContext = portfolioContext;
