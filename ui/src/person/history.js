a1376c7e97ca251bf5030ce3936d74376dd66f9a
Shawn Souto <shawn.souto@gmail.com>
2018-11-19 13:25:03 -0500

    build first version of history.html

--- ui/src/person/history.js
+++ ui/src/person/history.js
@@ -1,4 +1,23 @@
-//TODO: finish history page
-export default class History {
+import personContext from 'models/person';
+import navigationLinkContext from 'models/navigation-link';
+import positionContext from 'models/position';
+import _ from 'lodash';
 
+export default class History {
+  constructor() {
+    this.personContext = personContext;
+    this.navigationLinkContext = navigationLinkContext;
+    this.positionContext = positionContext;
+    this.person = {};
+    this.navigationLinks = [];
+    this.resumeLink = {};
+    this.positions = [];
+  }
+
+  created() {
+    this.person = this.personContext.getPerson(0);
+    this.navigationLinks = this.navigationLinkContext.getNavigationLinks(this.person.id);
+    this.resumeLink = _(this.navigationLinks).find(n => n.type === 'resume');
+    this.positions = this.positionContext.getPositions(this.person.id);
+  }
 }
