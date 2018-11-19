70dac517ad0edb6c5abcf00050d000b378b3d757
Shawn Souto <shawn.souto@gmail.com>
2018-11-18 23:01:15 -0500

    add next step tasks to portfolio

--- ui/src/person/portfolio.js
+++ ui/src/person/portfolio.js
@@ -8,6 +8,8 @@ export default class Portfolio {
     this.portfolioEntries = [];
   }
 
+  //TODO: make categories on portfolio page dynamic (All, Custom Software Platforms, Internet Marketing)
+  //TODO: make filtering by category work
   created() {
     this.person = this.personContext.getPerson(0);
     this.portfolioEntries = this.portfolioContext.getPortfolioEntries(this.person.id);
