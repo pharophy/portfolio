c9c8f0c899be24ce37865d702a471cb075bd0751
Shawn Souto <shawn.souto@gmail.com>
2018-11-18 23:00:09 -0500

    add customization to allow calendly urls per person and set up submit function on contact form to connect to BjxEngage

--- ui/src/person/contact.js
+++ ui/src/person/contact.js
@@ -1,7 +1,55 @@
-//import aurelia from 'aurelia-framework';
+import personContext from 'models/person';
 
 export default class Contact {
-  created() {
+  constructor() {
+    this.personContext = personContext;
+    this.person = {};
+  }
 
+  created() {
+    this.person = this.personContext.getPerson(0);
+  }
+
+  sendMessage() {
+    let formData = {
+      FormName: '[SSI Design] Person Contact Form',
+      FormID: 1,
+      FormDownloadUrl: 'https://www.blabberjax.com/thank-you.aspx',
+      FormFields: {
+        FirstName: document.getElementById('form_name').value,
+        LastName: document.getElementById('form_lastname').value,
+        EmailAddress: document.getElementById('form_email').value,
+        PhoneNumber: document.getElementById('form_phone').value,
+        CommentNotes: document.getElementById('form_message').value
+      }
+    };
+
+    //TODO: need to allow CORS for domain [svr side on blabberjax.com]
+    //TODO: set up result messages, not an alert
+    let request = $.ajax({
+      url: 'https://www.blabberjax.com/bjx_code/plugins/bjxEngage/Services/FormHandler.asmx/SubmitForm',
+      type: 'POST',
+      dataType: 'json',
+      crossDomain: true,
+      cache: false,
+      global: true,
+      data: '{ "form" : ' + JSON.stringify(formData) + ' }',
+      contentType: 'application/json; charset=utf-8'
+    });
+    request.done(function(n) {
+      if (n.d.RedirectUrl) {
+        window.location.href = n.d.RedirectUrl;
+      } else {
+        alert(n.d);
+      }
+      //BlabberJax.Engage.DisplayMessage(n.d, t);
+      //$(".bjx-engage-processing").css("display", "none")
+    });
+    request.fail(function(n) {
+      console.log(n);
+      alert(n.statusText);
+      //BlabberJax.Engage.DisplayMessage(n.responseJSON.d, t);
+      //$(".bjx-engage-processing").css("display", "none")
+    });
   }
 }
