f68ccef462260492ff69c337824ee4c691f62687
Shawn Souto <shawn.souto@gmail.com>
2018-11-19 13:18:11 -0500

    clean up line spacing

--- ui/src/models/skill.js
+++ ui/src/models/skill.js
@@ -17,7 +17,7 @@ export default class Skill extends NavigationLink {
 
   static getSkills(personId) {
     return [
-      
+
       new Skill('Team Leadership', '#/skill/team-leadership', 'skill', 'Roles', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 4, 1),
       new Skill('Enterprise Architect', '#/skill/enterprise-architect', 'skill', 'Roles', null, 'flaticon-insignia', 200, 'This is an architect description', null, null, 3, 2),
       new Skill('Software Architect', '#/skill/software-architect', 'skill', 'Roles', null, 'flaticon-insignia', 200, 'This is an architect description', null, null, 4, 3),
@@ -27,7 +27,7 @@ export default class Skill extends NavigationLink {
       new Skill('Internet Marketer', '#/skill/internet-marketer', 'skill', 'Roles', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 4, 7),
       new Skill('Product Manager', '#/skill/product-manager', 'skill', 'Roles', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 2, 8),
       new Skill('Agile Project Manager', '#/skill/agile-project-manager', 'skill', 'Roles', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 2, 9),
-      
+
       new Skill('Business Analysis', '#/skill/business-analysis', 'skill', 'Soft Skills', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 5, 10),
       new Skill('Presentation Skills', '#/skill/presentation-skill', 'skill', 'Soft Skills', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 4, 11),
       new Skill('Technical Strategy', '#/skill/technical-leader', 'skill', 'Soft Skills', null, 'flaticon-profile5', 100, 'This is a visionary description.', null, null, 4, 12),
@@ -43,7 +43,7 @@ export default class Skill extends NavigationLink {
       new Skill('Angular JS', '#/skill/ember-js', 'skill', 'Front-end', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 1, 21),
       new Skill('Knockout JS', '#/skill/knockout-js', 'skill', 'Front-end', null, 'flaticon-insignia', 200, 'This is an architect description', null, null, 3, 22),
       new Skill('jQuery', '#/skill/jquery', 'skill', 'Front-end', null, 'flaticon-insignia', 200, 'This is an architect description', null, null, 4, 23),
-      
+
       new Skill('Node JS', '#/skill/node-js', 'skill', 'Web Stack', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 3, 24),
       new Skill('Express JS', '#/skill/express-js', 'skill', 'Server-side Frameworks', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 3, 25),
       new Skill('CSS3', '#/skill/css3', 'skill', 'Front-end', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 3, 26),
@@ -60,13 +60,13 @@ export default class Skill extends NavigationLink {
 
       new Skill('MSSQL', '#/skill/mssql', 'skill', 'Database', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 4, 34),
       new Skill('SQL', '#/skill/sql', 'skill', 'Database', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 4, 35),
-      
+
       new Skill('Docker', '#/skill/docker', 'skill', 'DevOps', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 2, 36),
       new Skill('Kubernetes', '#/skill/kubernetes', 'skill', 'DevOps', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 1, 37),
-      
+
       new Skill('Google Cloud Platform', '#/skill/google-cloud-platform', 'skill', 'Cloud', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 2, 38),
       new Skill('Azure AD B2C', '#/skill/azure', 'skill', 'Cloud', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 1, 39),
-      
+
       new Skill('Progressive Web Apps', '#/skill/progressive-web-apps', 'skill', 'Front-end', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 1, 40),
       new Skill('Single Page Apps', '#/skill/single-page-apps', 'skill', 'Front-end', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 4, 41),
 
@@ -75,18 +75,18 @@ export default class Skill extends NavigationLink {
       new Skill('SAP HANA XS', '#/skill/sap-hana-xs', 'skill', 'SAP', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 4, 44),
       new Skill('SAP ABAP', '#/skill/sap-abap', 'skill', 'SAP', null, 'flaticon-insignia', 200, 'This is an architect description', null, null, 3, 45),
       new Skill('SAPUI5', '#/skill/sapui5', 'skill', 'SAP', null, 'flaticon-insignia', 200, 'This is an architect description', null, null, 1, 46),
-      
+
       new Skill('Qlikview', '#/skill/qlikview', 'skill', 'Dashboards', null, 'flaticon-insignia', 200, 'This is an architect description', null, null, 2, 47),
       new Skill('Tableau', '#/skill/tableau', 'skill', 'Dashboards', null, 'flaticon-insignia', 200, 'This is an architect description', null, null, 1, 48),
 
       new Skill('git', '#/skill/git', 'skill', 'DevOps', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 3, 49),
       new Skill('GitHub', '#/skill/github', 'skill', 'DevOps', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 3, 50),
-      
+
       new Skill('SEO', '#/skill/seo', 'skill', 'Marketing', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 4, 51),
       new Skill('Email Marketing', '#/skill/email-marketing', 'skill', 'Marketing', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 4, 52),
       new Skill('Social Media Marketing', '#/skill/social-media-marketing', 'skill', 'Marketing', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 3, 53),
       new Skill('Content Marketing', '#/skill/content-marketing', 'skill', 'Marketing', null, 'flaticon-paper40', 400, 'This is a marketer description', null, null, 5, 54)
-      
+
     ];
   }
 }
