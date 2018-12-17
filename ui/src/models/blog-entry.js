56cec6e74cfe46903cd4d653511256879ddad2ac
Shawn Souto <shawn.souto@gmail.com>
2018-12-17 11:05:29 -0500

    make blog entry loading dynamic from blabberjax.com

--- ui/src/models/blog-entry.js
+++ ui/src/models/blog-entry.js
@@ -1,27 +1,43 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
 export default class BlogEntry {
-  constructor(title, url = '#',  imageUrl, shortDescription, id = 0) {
+  constructor(title, url = '#',  imageUrl, shortDescription, pubDate, id = 0) {
     this.id = id;
     this.title = title;
     this.url = url;
     this.imageUrl = imageUrl;
     this.shortDescription = shortDescription;
     this.id = id;
+    this.publishDate = pubDate;
   }
 
-  static getBlogEntries(personId) {
-    return [
-      new BlogEntry(
-        'How to Create Effective Content for Inbound Marketing',
-        'https://www.blabberjax.com/content-marketing/how-to-create-effective-content-for-inbound-marketing/',
-        'https://www.blabberjax.com/uploads/inbound-marketing-content-creation-90-a.jpg',
-        'If only you knew how to generate content for inbound marketing, your website could be working harder, driving more traffic, and converting more sales. But how do you generate a never-ending stream of content ideas?'
-      ),
-      new BlogEntry(
-        'How to Market to Influencers on Instagram',
-        'https://www.blabberjax.com/social-media-marketing/how-to-market-to-influencers-on-instagram/',
-        'https://www.blabberjax.com/uploads/instagram-influencer-marketing-89-a.jpg',
-        'Did you know that instagram influencer marketing can bring you up to 11x return on investment?'
-      )
-    ];
+  static async getBlogEntries(personId) {
+    let feed = 'https://www.blabberjax.com/rss.xml';
+
+    this.blogEntries = [];
+    let rssItemsResult = await $.ajax(feed, {
+      accepts: {
+        xml: 'application/rss+xml'
+      },
+      dataType: 'xml',
+      crossDomain: true
+    });
+    let rssItems = $(rssItemsResult).find('item');
+    for (const item of rssItems) {
+      const el = $(item);
+      this.blogEntries.push(
+        new BlogEntry(
+          el.find('title').text(),
+          el.find('link').text(),
+          el.find('enclosure').attr('url'),
+          el.find('description').text(),
+          new Date(el.find('pubDateParsed').text())
+        )
+      );
+    }
+    this.blogEntries = _(this.blogEntries).orderBy(['publishDate'], ['desc']).take(12).value();
+
+    return this.blogEntries;
   }
 }
