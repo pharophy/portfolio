2f2a858ce162c69e0a0822bc2a5dca3ae9eaa74f
Shawn Souto <shawn.souto@gmail.com>
2018-12-17 16:36:21 -0500

    add new components for spinners

---
+++ ui/src/components/_shared/spinner-inline.js
@@ -0,0 +1,29 @@
+import { customElement, bindable, bindingMode } from 'aurelia-framework';
+
+@customElement('spinner-inline')
+export class SpinnerInline {
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) loading;
+
+  constructor(loading) {
+    this.loading = loading;
+  }
+
+  loadingChanged(newStatus) {
+    this.loading = newStatus;
+    this.bind();
+  }
+
+  attached() {
+    $(document).ready(() => {
+      this.bind();
+    });
+  }
+
+  bind() {
+    if (this.loading) {
+      $('#busySpinner').show();
+    } else {
+      $('#busySpinner').hide();
+    }
+  }
+}
