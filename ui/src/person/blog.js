b230a8fe13006d9b44b6f6a284090a4edbf203e1
Shawn Souto <shawn.souto@gmail.com>
2018-12-17 16:37:41 -0500

    adjust loading from await style to promise.then to allow loading spinner

--- ui/src/person/blog.js
+++ ui/src/person/blog.js
@@ -1,19 +1,25 @@
-import { bindable } from 'aurelia-framework';
+import { bindable, bindingMode } from 'aurelia-framework';
 import personContext from 'models/person';
 import blogContext from 'models/blog-entry';
 
-export default class Blog {
+export class Blog {
   @bindable blogEntries;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) loading;
 
   constructor() {
     this.personContext = personContext;
     this.blogContext = blogContext;
     this.person = {};
     this.blogEntries = [];
+    this.loading = true;
   }
 
   async activate() {
     this.person = this.personContext.getPerson(0);
-    this.blogEntries = await this.blogContext.getBlogEntries(this.person.id);
+    let queryPromise = this.blogContext.getBlogEntries(this.person.id);
+    queryPromise.then(data => {
+      this.blogEntries = data;
+      this.loading = false;
+    });
   }
 }
