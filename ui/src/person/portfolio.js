36924f1d3319dfc3ce495a2281192d61f5e9ab77
Shawn Souto <shawn.souto@gmail.com>
2018-12-18 20:20:36 -0500

    clean up portfolio.js and handle images not loaded breaking isotope layout

--- ui/src/person/portfolio.js
+++ ui/src/person/portfolio.js
@@ -1,39 +1,29 @@
 import personContext from 'models/person';
 import portfolioContext from 'models/portfolio-entry';
 let Isotope = require('isotope-layout');
-//import PortfolioCollection from 'models/portfolio-collection';
+let imagesLoaded = require('imagesloaded');
 
 export default class Portfolio {
   constructor() {
     this.personContext = personContext;
     this.portfolioContext = portfolioContext;
-    //this.portfolioCollection = new PortfolioCollection();
     this.person = {};
     this.portfolioEntries = [];
-    //this.$grid = {};
   }
 
-  //TODO: make categories on portfolio page dynamic (All, Custom Software Platforms, Internet Marketing)
-  //TODO: make filtering by category work
   async created() {
     this.person = this.personContext.getPerson(0);
     this.portfolioEntries = await this.portfolioContext.getPortfolioEntries(this.person.id);
-    //this.portfolioEntries = await this.portfolioCollection.load();
   }
 
   async attached() {
     $(document).ready(() => {
-      window.$grid  = new Isotope(document.getElementById('portfolio-grid'), {
+      window.$grid = new Isotope(document.getElementById('portfolio-grid'), {
         itemSelector: '.grid-item',
         layoutMode: 'masonry'
       });
-      /*$grid = $('#portfolio-grid').isotope({
-        itemSelector: '.grid-item',
-        layoutMode: 'masonry'
-      });*/
       $('#isotope-filters').on( 'click', 'a', function(e) {
-        //this.$grid.isotope({ filter: $(this).attr('data-filter') });
-        $('.iso-filter').removeClass("active");
+        $('.iso-filter').removeClass('active');
         $(this).addClass('active');
 
         const filterText = $(this).attr('data-filter');
@@ -51,6 +41,10 @@ export default class Portfolio {
           });
         }
         e.preventDefault();
+      });
+      //layout after all images have loaded, fixes https://isotope.metafizzy.co/layout.html#imagesloaded
+      imagesLoaded($grid.element).on('done', function() {
+        window.$grid.layout();
       });
     });
   }
