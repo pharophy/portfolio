3c77a9c75c50d5b25c7a937d999fce55945b756a
Shawn Souto <shawn.souto@gmail.com>
2018-12-19 13:55:12 -0500

    connect footer form to BjxContacts

--- ui/src/components/person/person-footer.js
+++ ui/src/components/person/person-footer.js
@@ -20,4 +20,41 @@ export class PersonFooter {
     this.socialLinks = socLinks; // _(socLinks).keyBy('subtype');
   }
 
+  signUp() {
+    let formData = {
+      FormName: '[SSI Design] Newsletter Signup Form',
+      FormID: 1,
+      FormDownloadUrl: 'https://www.blabberjax.com/thank-you.aspx',
+      FormFields: [
+        { FieldName: 'EmailAddress', FieldValue: document.getElementById('newsl_form_email').value }
+      ]
+    };
+
+    //TODO: set up result messages, not an alert
+    let request = $.ajax({
+      url: 'https://www.blabberjax.com/bjx_code/plugins/bjxEngage/Services/FormHandler.asmx/SubmitForm',
+      type: 'POST',
+      dataType: 'json',
+      crossDomain: true,
+      cache: false,
+      global: true,
+      data: '{ "form" : ' + JSON.stringify(formData) + ' }',
+      contentType: 'application/json; charset=utf-8'
+    });
+    request.done(function(n) {
+      if (n.d.RedirectUrl) {
+        window.location.href = n.d.RedirectUrl;
+      } else {
+        alert(n.d);
+      }
+      //BlabberJax.Engage.DisplayMessage(n.d, t);
+      //$(".bjx-engage-processing").css("display", "none")
+    });
+    request.fail(function(n) {
+      console.log(n);
+      alert(n.statusText);
+      //BlabberJax.Engage.DisplayMessage(n.responseJSON.d, t);
+      //$(".bjx-engage-processing").css("display", "none")
+    });
+  }
 }
